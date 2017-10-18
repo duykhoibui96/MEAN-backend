1f32c5cd2cbb51dd8064bdcab633e769831e2b6d
buiduykhoi <duykhoi.bui96@gmail.com>
2017-10-18 09:35:53 +0700

    Set CORS

--- app.js
+++ app.js
@@ -8,15 +8,23 @@ var api = require('./routes/api');
 
 var app = express();
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+    next();
+});
+
 // view engine setup
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 
-app.use('/api',api);
+app.use('/api', api);
 
 module.exports = app;
